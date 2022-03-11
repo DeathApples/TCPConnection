e5833aad2c55f86739c583eb929230c524a3f7f7
BlackFox <fiona.speranskih@yandex.ru>
2022-03-11 17:49:51 +0500

    Добавьте файлы проекта.

---
+++ Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int port = 52000;
+            var data = new byte[1024];
+            const string ip = "127.0.0.1";
+
+            Console.Write("Введите имя файла, который хотите сохранить на сервере: ");
+            string name = Console.ReadLine();
+
+            var endPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(endPoint);
+
+            data[0] = (byte)name.Length;
+            Encoding.UTF8.GetBytes(name, 0, name.Length, data, 1);
+
+            socket.Send(data);
+
+            using (var fstream = new FileStream(name, FileMode.Open))
+            {
+                while (fstream.Read(data, 0, data.Length) > 0)
+                {
+                    socket.Send(data);
+                }
+            }
+
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static int size;
+        static Socket socket;
+        static IPEndPoint endPoint;
+        
+        const int port = 52000;
+        const string ip = "127.0.0.1";
+        static byte[] data = new byte[1024];
+        static StringBuilder name = new StringBuilder();
+
+        static void Main(string[] args)
+        {
+            Initialise();
+            Listening();
+        }
+
+        static void Initialise()
+        {
+            endPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Bind(endPoint);
+            socket.Listen(5);
+        }
+
+        static void Listening()
+        {
+            while (true)
+            {
+                var listener = socket.Accept();
+
+                listener.Receive(data);
+
+                name.Append(Encoding.UTF8.GetString(data, 1, data[0]));
+                Console.WriteLine(name);
+
+                using (FileStream fstream = new FileStream(name.ToString(), FileMode.OpenOrCreate))
+                {
+                    do
+                    {
+                        size = listener.Receive(data);
+                        fstream.Write(data, 0, size);
+
+                    } while (listener.Available > 0);
+                }
+
+                name.Clear();
+
+                listener.Shutdown(SocketShutdown.Both);
+                listener.Close();
+            }
+        }
+    }
+}
